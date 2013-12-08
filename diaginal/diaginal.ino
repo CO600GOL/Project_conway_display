ec8bb45819cb1bd31d435da70bb81fd1f1557c29
Richard Lancaster <rl221@kent.ac.uk>
2013-12-08 22:29:25 +0000

    Added script that lights upa diaginal line

---
+++ diaginal/diaginal.ino
@@ -0,0 +1,63 @@
+#include "SPI.h"
+#include "Adafruit_WS2801.h"
+
+/*****************************************************************************
+Example sketch for driving Adafruit WS2801 pixels!
+
+
+  Designed specifically to work with the Adafruit RGB Pixels!
+  12mm Bullet shape ----> https://www.adafruit.com/products/322
+  12mm Flat shape   ----> https://www.adafruit.com/products/738
+  36mm Square shape ----> https://www.adafruit.com/products/683
+
+  These pixels use SPI to transmit the color data, and have built in
+  high speed PWM drivers for 24 bit color per pixel
+  2 pins are required to interface
+
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by David Kavanagh (dkavanagh@gmail.com).  
+  BSD license, all text above must be included in any redistribution
+
+*****************************************************************************/
+
+// Choose which 2 pins you will use for output.
+// Can be any valid output pins.
+// The colors of the wires may be totally different so
+// BE SURE TO CHECK YOUR PIXELS TO SEE WHICH WIRES TO USE!
+uint8_t dataPin  = 2;    // Yellow wire on Adafruit Pixels
+uint8_t clockPin = 3;    // Green wire on Adafruit Pixels
+
+// Don't forget to connect the ground wire to Arduino ground,
+// and the +5V wire to a +5V supply
+
+// Set the first variable to the NUMBER of pixels in a row and
+// the second value to number of pixels in a column.
+Adafruit_WS2801 strip = Adafruit_WS2801((uint16_t)5, (uint16_t)5, dataPin, clockPin);
+
+void setup() {
+  uint32_t x, y;
+  uint32_t w = 5;
+  uint32_t h = 5;
+  
+  strip.begin();
+
+  // Update LED contents, to start they are all 'off'
+  strip.show();
+  
+  // Set (0, 0) 
+  for (x=0; x<w; x++){
+    for (y=0; y<h; y++){
+      strip.setPixelColor(x, 0, 255, 0, 0);    
+    }  
+  }
+  strip.show();
+}
+
+
+void loop() {
+  while (true){};
+}
+
